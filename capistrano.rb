3443230a863543e40de1cce1be61aab4e7be06ee
Andrew Forward <aforward@gmail.com>
2014-07-28 17:17:30 -0400

    Added rync capabilities between local computer and remote server

---
+++ capistrano.rb
@@ -0,0 +1,47 @@
+
+if ENV['TARGET'].nil? || ENV['USER'].nil?
+  puts "Please specify target 'TARGET=<remote_host>', e.g. 'TARGET=10.0.0.3'\n" if ENV['TARGET'].nil?
+  puts "Please specify user 'USER=<user>', e.g. 'USER=root'\n" if ENV['USER'].nil?
+  puts ""
+  exit
+end
+
+role :target, "#{ENV['USER']}@#{ENV['TARGET']}"
+set :stage, :production
+
+cwd = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+capbash_dir = '/var/capbash'
+
+namespace :capbash do
+
+  desc "Deploy to your server"
+  task :deploy do
+    invoke 'capbash:install_rsync'
+    invoke 'capbash:sync_capbash'
+  end
+
+  desc "Install Cookbook Repository from cwd"
+  task :install_rsync do
+    on roles(:target), in: :sequence, wait: 1 do
+      execute 'aptitude install -y rsync' unless test("which rsync")
+      execute "mkdir -m 0775 -p #{capbash_dir}" if test("[ ! -e #{capbash_dir} ]")
+    end
+  end
+
+  desc "Re-install Cookbook Repository from cwd"
+  task :sync_capbash do
+    run_locally do
+      execute "rsync -avz --delete -e \"ssh -p22\" \"#{cwd}/\" \"#{ENV['USER']}@#{ENV["TARGET"]}:#{capbash_dir}\" --exclude \".svn\" --exclude \".git\""
+    end
+  end
+
+
+
+  desc "Remove all traces of capbash"
+  task :cleanup do
+    on roles(:target), in: :sequence, wait: 1 do
+      execute "rm -rf #{capbash_dir}"
+    end
+  end
+
+end
